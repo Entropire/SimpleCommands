5ace96d3ecfac89545af84721a4f2a8b6d9d66e3
Entropire <158569522+Entropire@users.noreply.github.com>
2025-10-09 11:25:21 +0200

    allowed to asign multiple names to a command with the CommandName attribute

--- SimpleCommands/CommandNameAttribute .cs
+++ SimpleCommands/CommandNameAttribute .cs
@@ -8,19 +8,19 @@
   /// the name under which the command will be registered and executed.
   /// The name must be unique across all registered commands.
   /// </remarks>
-  [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+  [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
   public class CommandNameAttribute : Attribute
   {
     /// <summary>
     /// Gets the name of the command.
     /// </summary>
-    public string Name { get; }
+    public string[] Name { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CommandNameAttribute"/> class with the specified command name.
     /// </summary>
     /// <param name="name">The unique name of the command.</param>
-    public CommandNameAttribute(string name)
+    public CommandNameAttribute(string[] name)
     {
       Name = name;
     }
