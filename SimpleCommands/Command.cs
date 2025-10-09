a25d06e3b2ff30413ae94ae521e6f6a56e94469a
Entropire <158569522+Entropire@users.noreply.github.com>
2025-10-09 11:52:29 +0200

    removed comments and finished the system where multiple names can be asigned to a command via the CommandName attribute

--- SimpleCommands/Command.cs
+++ SimpleCommands/Command.cs
@@ -2,29 +2,10 @@
 
 namespace SimpleCommands
 {
-  /// <summary>
-  /// Represents a base class for all commands that can be registered 
-  /// and executed via <see cref="CommandHandler"/>.
-  /// </summary>
-  /// <remarks>
-  /// Each subclass must be decorated with a <see cref="CommandNameAttribute"/>,
-  /// which defines the unique name under which the command will be registered.
-  /// The <see cref="Name"/> property is automatically populated from the attribute.
-  /// </remarks>
   public abstract class Command
   {
-    /// <summary>
-    /// Gets the unique name of the command, as specified by <see cref="CommandNameAttribute"/>.
-    /// </summary>
-    public string Name { get; }
+    public string[] Names { get; }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Command"/> class and
-    /// ensures the derived class has a <see cref="CommandNameAttribute"/>.
-    /// </summary>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if the derived class does not have a <see cref="CommandNameAttribute"/>.
-    /// </exception>
     protected Command()
     {
       var attribute = GetType().GetCustomAttribute<CommandNameAttribute>();
@@ -35,15 +16,9 @@ namespace SimpleCommands
             $"Class '{GetType().FullName}' must have a [CommandName] attribute.");
       }
 
-      Name = attribute.Name;
+      Names = attribute.Names;
     }
 
-    /// <summary>
-    /// Executes the command logic.
-    /// </summary>
-    /// <param name="args">
-    /// The arguments passed to the command during execution.
-    /// </param>
     public abstract void Execute(string CommandName, string[] args);
   }
 }
--- SimpleCommands/CommandHandler.cs
+++ SimpleCommands/CommandHandler.cs
@@ -1,38 +1,10 @@
 ﻿namespace SimpleCommands
 {
-  /// <summary>
-  /// Provides a simple command management system that allows you to 
-  /// register, execute, and unregister commands at runtime.
-  /// </summary>
-  /// <remarks>
-  /// Commands can be registered either by specifying a unique name and a 
-  /// delegate (<see cref="Action{T}"/>), or by providing a class that 
-  /// inherits from <see cref="Command"/> and is decorated with a 
-  /// <see cref="CommandNameAttribute"/>.
-  /// <para>
-  /// This enables flexible command handling where commands can be 
-  /// dynamically added, removed, or executed based on their names.
-  /// </para>
-  /// </remarks>
   public class CommandHandler
   {
     private static Dictionary<string, Action<string, string[]>> commandList = new Dictionary<string, Action<string, string[]>>();
     private string commandPrefix = "/";
 
-    /// <summary>
-    /// Registers a new command with the <see cref="CommandHandler"/>.
-    /// </summary>
-    /// <param name="name">
-    /// The unique name of the command. This is the identifier used to 
-    /// execute or unregister the command later.
-    /// </param>
-    /// <param name="action">
-    /// The delegate (callback) that will be executed when the command 
-    /// is invoked. The string array contains the arguments passed at runtime.
-    /// </param>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if a command with the same name has already been registered.
-    /// </exception>
     public void Register(string name, Action<string, string[]> action)
     {
       name = name.ToLower();
@@ -46,40 +18,21 @@
       }
     }
 
-    /// <summary>
-    /// Registers a new command instance with the <see cref="CommandHandler"/>.
-    /// </summary>
-    /// <param name="command">
-    /// The <see cref="Command"/> instance to register. The command must have a 
-    /// <see cref="CommandNameAttribute"/> applied to its class in order to determine 
-    /// the unique name under which it will be registered.
-    /// </param>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if the command does not have a <see cref="CommandNameAttribute"/> 
-    /// or if a command with the same name has already been registered.
-    /// </exception>
     public void Register(Command command)
     {
-      string name = command.Name.ToLower();
-      if (!commandList.ContainsKey(name))
+      foreach (string name in command.Names)
       {
-        commandList.Add(name, command.Execute);
-      }
-      else
-      {
-        throw new InvalidOperationException($"Command '{command.Name}' already exists.");
+        if (!commandList.ContainsKey(name))
+        {
+          commandList.Add(name, command.Execute);
+        }
+        else
+        {
+          throw new InvalidOperationException($"Command '{name}' already exists.");
+        }
       }
     }
 
-    /// <summary>
-    /// Unregisters a previously registered command from the <see cref="CommandHandler"/>.
-    /// </summary>
-    /// <param name="name">
-    /// The unique name of the command to unregister
-    /// </param>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if the specified command does not exist in the handler.
-    /// </exception>
     public void Unregister(string name) {
       name = name.ToLower();
       if (commandList.ContainsKey(name)) {
@@ -91,18 +44,6 @@
       }
     }
 
-/// <summary>
-/// Executes a registered command by its name.
-/// </summary>
-/// <param name="name">
-/// The unique name of the command to execute. Must match the name used when the command was registered.
-/// </param>
-/// <param name="args">
-/// The arguments to pass to the command’s execution logic. Can be an empty array if the command does not require arguments.
-/// </param>
-/// <exception cref="InvalidOperationException">
-/// Thrown if the specified command does not exist in the <see cref="CommandHandler"/>
-/// </exception>
     public void Execute(string name, string[] args)
     {
       if (commandList.TryGetValue(name, out Action<string, string[]>? action))
@@ -115,15 +56,6 @@
       }
     }
 
-    /// <summary>
-    /// Executes a registered command by its name.
-    /// </summary>
-    /// <param name="name">
-    /// The users input
-    /// </param>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if the specified command does not exist in the <see cref="CommandHandler"/>
-    /// </exception>
     public void Execute(string userInput)
     {
       if (!userInput.StartsWith(commandPrefix)) return;
@@ -142,12 +74,6 @@
       }
     }
 
-    /// <summary>
-    /// Set the prefix used to execute the commands in this registry
-    /// </summary>
-    /// <param name="commandPrefix">
-    /// The prefix you want to use to execute the command in this registry
-    /// </param>
     public void SetCommandPrefix(string commandPrefix)
     {
       this.commandPrefix = commandPrefix;
