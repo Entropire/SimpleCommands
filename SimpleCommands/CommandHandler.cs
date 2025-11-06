73db1c95932121a6df96346aa774b72bfacd10e8
Entropire <158569522+Entropire@users.noreply.github.com>
2025-11-06 12:42:58 +0100

    Refactor Execute methods to TryExecute with bool return

--- SimpleCommands/CommandHandler.cs
+++ SimpleCommands/CommandHandler.cs
@@ -2,39 +2,41 @@
 
 public class CommandHandler
 {
-  private CommandRegistry _commandRegistry;
-  private CommandInputParser _commandInputParser;
+    private CommandRegistry _commandRegistry;
+    private CommandInputParser _commandInputParser;
 
-  public CommandHandler(string commandPrefix)
-  {
-    _commandRegistry = new CommandRegistry();
-    _commandInputParser = new CommandInputParser(commandPrefix);
-  }
-
-  public void RegisterCommand(Action<string, string[]> commandAction, params string[] commandNames) 
-    => _commandRegistry.RegisterCommand(commandAction, commandNames);
-
-  public void RegisterCommand(Command command) => _commandRegistry.RegisterCommand(command);
-
-  public void UnregisterCommand(string commanName) => _commandRegistry.UnregisterCommand(commanName);
-
-  public void Execute(string name, string[] args)
-  {
-     if (!_commandRegistry.TryGetCommand(name, out Action<string, string[]>? commandAction))
+    public CommandHandler(string commandPrefix)
     {
-      return;   
+        _commandRegistry = new CommandRegistry();
+        _commandInputParser = new CommandInputParser(commandPrefix);
     }
 
-    commandAction?.Invoke(name, args);
-  }
+    public void RegisterCommand(Action<string, string[]> commandAction, params string[] commandNames)
+      => _commandRegistry.RegisterCommand(commandAction, commandNames);
 
-  public void Execute(string userInput)
-  {
-    if (!_commandInputParser.TryParseUserInput(userInput, out string commandName, out string[] commandArgs))
+    public void RegisterCommand(Command command) => _commandRegistry.RegisterCommand(command);
+
+    public void UnregisterCommand(string commanName) => _commandRegistry.UnregisterCommand(commanName);
+
+    public bool TryExecute(string name, string[] args)
     {
-      return;
+        if (!_commandRegistry.TryGetCommand(name, out Action<string, string[]>? commandAction))
+        {
+            return false;
+        }
+
+        commandAction?.Invoke(name, args);
+        return true;
     }
 
-    Execute(commandName, commandArgs);
-  }
+    public bool TryExecute(string userInput)
+    {
+        if (!_commandInputParser.TryParseUserInput(userInput, out string commandName, out string[] commandArgs))
+        {
+            return false;
+        }
+
+        TryExecute(commandName, commandArgs);
+        return true;
+    }
 }
